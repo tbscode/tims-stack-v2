5795b070b54ed52d5f7274852beb789dbeef04a0
tim <tim@timschupp.de>
2023-09-26 15:24:09 +0200

    matomo and maria db basic setup complete

--- matomo/html/tmp/cache/tracker/matomocache_allSiteUrlsPerSite.php
+++ matomo/html/tmp/cache/tracker/matomocache_allSiteUrlsPerSite.php
@@ -1,1 +1,1 @@
-<?php return unserialize('a:2:{s:8:"lifetime";i:1695732420;s:4:"data";a:1:{i:1;a:1:{i:0;s:33:"https://host.docker.internal:8000";}}}', ['allowed_classes' => false]);
+<?php return unserialize('a:2:{s:8:"lifetime";i:1695736194;s:4:"data";a:1:{i:1;a:1:{i:0;s:33:"https://host.docker.internal:8000";}}}', ['allowed_classes' => false]);
